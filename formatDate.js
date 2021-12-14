9856c6ef5903f06042a9c3ec6977f591992f72cf
zsl-157 <1522192221@qq.com>
2021-12-14 16:12:09 +0800

    时间格式化工具第一次提交

---
+++ formatDate.js
@@ -0,0 +1,17 @@
+function formatDate(value){
+				var index = 0;
+				var underLineIndex = 0;
+				while(index<value.length){
+				  if(value[index] == '-'){
+				    underLineIndex ++;
+				    if(underLineIndex == 1){
+				      value = value.replace(value[index],"年")}
+				    if(underLineIndex == 2){
+				      value = value.replace(value[index],"月")}
+				  }
+				  index ++;
+				}
+				value += "日";
+				return value;
+			}
+console.log(formatDate("2021-12-15"))
